141057dc1a57ac667e850d87b613b0baad55e91d
Sohee <sohee3480@gmail.com>
2021-05-22 16:56:05 +0900

    액션바에 버튼 추가 -> 액티비티로 가져와서 -> 토스트 등 이벤트 붙여보기

--- app/src/main/java/com/example/drawablexmlpractice_sohee/MainActivity.kt
+++ app/src/main/java/com/example/drawablexmlpractice_sohee/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.drawablexmlpractice_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+//    액션바에 있는 UI를 담아둘 변수
+//    변수 : 멤버 변수 => 담아주는 행위 : onCreate
+    lateinit var toastBtn : Button
+
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,5 +30,14 @@ class MainActivity : AppCompatActivity() {
 //       커스텀뷰를 들고 있는 툴바를 변수로 저장
         val toolbar = defaultActionBar.customView.parent as androidx.appcompat.widget.Toolbar
         toolbar.setContentInsetsAbsolute(0, 0)
+
+
+//       커스텀 액션바에 달아둔 버튼을 찾아오기
+       toastBtn = defaultActionBar.customView.findViewById(R.id.toastBtn)
+       
+       toastBtn.setOnClickListener {
+           Toast.makeText(this, "액션바를 통한 토스트", Toast.LENGTH_SHORT).show()
+       }
+
     }
 }
